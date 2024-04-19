9674799e4926291881f2389f93801341b1128a13
andreascmbuch <anbu0001@stud.kea.dk>
2024-04-19 15:21:43 +0200

    Changes to main and welcome screen

--- app/src/main/java/com/example/myapplication/Welcomescreen.kt
+++ app/src/main/java/com/example/myapplication/Welcomescreen.kt
@@ -1,2 +1,21 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Welcome() {
+    Box() {
+        Column {
+            Text(text = "Welcome to Strume")
+            // Button for navigation to homepage
+            Button(onClick = { /*TODO*/ }) {
+            }
+            Text(text = "Click here to continue")
+        }
+
+    }
+}
