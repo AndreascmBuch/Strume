c9c885463f0a7aa38f7ce958de9c02eeaa3ae4bb
NikoJulius1 <niju0001@stud.kea.dk>
2024-04-23 17:00:14 +0200

    Lavet + knap

--- app/src/main/java/com/example/myapplication/Homescreen.kt
+++ app/src/main/java/com/example/myapplication/Homescreen.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,5 +28,13 @@ fun HomeScreen(username: String) {
     ) {
         Text(text = "Hello $username", color = Color.White)
         Text(text = "Here are your tasks for the day", color = Color.White)
+        //Knap til tilføjelse
+        Button(
+            onClick = { Log.d("HomeScreen", "Button Clicked!") },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+        ) {
+            Text(text = "+", color = Color.White)
+        }
+
     }
 }
