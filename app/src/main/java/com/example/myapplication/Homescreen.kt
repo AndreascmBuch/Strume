837eae96f1c3e62ed9c9d9e979250586a5e27752
NikoJulius1 <niju0001@stud.kea.dk>
2024-04-23 17:34:30 +0200

    Gjort så knap tilføjer ord til liste

--- app/src/main/java/com/example/myapplication/Homescreen.kt
+++ app/src/main/java/com/example/myapplication/Homescreen.kt
@@ -6,14 +6,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
+
 
 
 @Composable
@@ -28,9 +33,20 @@ fun HomeScreen(username: String) {
     ) {
         Text(text = "Hello $username", color = Color.White)
         Text(text = "Here are your tasks for the day", color = Color.White)
+        
+        //Den state der håndterer task listen
+        val tasks = remember { mutableStateListOf<String>() }
+
+        //Tom liste til at display user tasks
+        LazyColumn{
+            items(tasks) { task: String ->
+                Text(text = task, color = Color.White, modifier = Modifier.padding(8.dp))
+            }
+        }
+
         //Knap til tilføjelse
         Button(
-            onClick = { Log.d("HomeScreen", "Button Clicked!") },
+            onClick = { tasks.add("Task 1") },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
         ) {
             Text(text = "+", color = Color.White)
@@ -38,3 +54,4 @@ fun HomeScreen(username: String) {
 
     }
 }
+
