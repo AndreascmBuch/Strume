e9e7bfa63e4b42ad72353dee4da4ddaa5b5ada56
NikoJulius1 <niju0001@stud.kea.dk>
2024-05-24 16:52:03 +0200

    Omskrevet så den har en date og timepicker i addtaskdialog

--- app/src/main/java/com/example/myapplication/homescreen/TaskDatabase.kt
+++ app/src/main/java/com/example/myapplication/homescreen/TaskDatabase.kt
@@ -1,15 +1,11 @@
 package com.example.myapplication.homescreen
 
-
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.myapplication.homescreen.Task
-import com.example.myapplication.homescreen.TaskDao
 
 @Database(
     entities = [Task::class],
-    version = 1
-)
-abstract class TaskDatabase: RoomDatabase() {
+    version = 1)
+abstract class TaskDatabase : RoomDatabase() {
     abstract val dao: TaskDao
 }
