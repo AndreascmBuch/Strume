91f5d6dec354dcb3a4fa26de2b6b69e028d305c0
emduma <dupont.magnussen@gmail.com>
2024-05-25 21:42:03 +0200

    Restructured code in order to add hasTask val, which makes sure only dates with tasks are bordered

--- app/src/main/java/com/example/myapplication/Calandar.kt
+++ app/src/main/java/com/example/myapplication/Calandar.kt
@@ -27,15 +27,16 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Calendar(state:TaskState) {
+fun Calendar(state: TaskState) {
     var selectedDate by remember { mutableIntStateOf(-1) }
     val weekdays = listOf("mon", "tue", "wed", "thu", "fri", "sat", "sun")
 
-    // Bottom sheet state functionality, https://developer.android.com/develop/ui/compose/components/bottom-sheets
+    // Bottom sheet state functionality
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     var showBottomSheet by remember { mutableStateOf(false) }
 
+    // Calendar layout
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -115,8 +116,7 @@ fun Calendar(state:TaskState) {
             }
 
             val daysInMonth = 30
-            val firstDayOffset =
-                5 // June 1st is a Saturday, so offset is 5 (0-based index for Saturday)
+            val firstDayOffset = 5 // June 1st is a Saturday, so offset is 5 (0-based index for Saturday)
             for (week in 0 until 6) {
                 Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
@@ -127,6 +127,11 @@ fun Calendar(state:TaskState) {
                     for (day in 0 until 7) {
                         val date = week * 7 + day - firstDayOffset + 1
                         if (date in 1..daysInMonth) {
+                            // hasTask checks if any task value is present for a date
+                            val hasTask = state.task.any {
+                                val dayOfMonth = "\\d+".toRegex().find(it.date)?.value?.toIntOrNull()
+                                dayOfMonth == date
+                            }
                             Box(
                                 contentAlignment = Alignment.Center,
                                 modifier = Modifier
@@ -137,7 +142,8 @@ fun Calendar(state:TaskState) {
                                     .background(if (selectedDate == date) Color(0x8021B6E8) else Color.Transparent)
                                     .border(
                                         width = 2.dp,
-                                        color = if (selectedDate != date) Color(0x3B21B6E8) else Color.Transparent,
+                                        // If task value is present, then border, else transparent
+                                        color = if (hasTask) Color(0x9921B6E8) else Color.Transparent,
                                         shape = CircleShape
                                     )
                                     .clickable {
@@ -183,8 +189,8 @@ fun Calendar(state:TaskState) {
                         .padding(16.dp)
                         .background(Color(0xFF4E4853), shape = RoundedCornerShape(16.dp))
                 ) {
-                    // Bottom Sheet content for each date
-                    LazyColumn(  // Made the content scrollable
+                    // Layout of Bottom Sheet content for each date
+                    LazyColumn(
                         verticalArrangement = Arrangement.Top,
                         horizontalAlignment = Alignment.Start
                     ) {
@@ -198,20 +204,20 @@ fun Calendar(state:TaskState) {
                                 Box(
                                     modifier = Modifier
                                         .weight(1f)
-                                        .padding(end = 16.dp)  // Add more padding to the right of task name
+                                        .padding(end = 16.dp)
                                 ) {
                                     Text(
                                         text = task.name,
                                         fontSize = 20.sp,
                                         fontWeight = FontWeight.Bold,
                                         color = Color.White,
-                                        modifier = Modifier.padding(start = 16.dp)  // More padding to the left of task name
+                                        modifier = Modifier.padding(start = 16.dp)
                                     )
                                 }
                                 Box(
                                     modifier = Modifier
                                         .align(Alignment.CenterVertically)
-                                        .padding(end = 24.dp)  // Add padding to move text more to the left
+                                        .padding(end = 24.dp)
                                 ) {
                                     Text(
                                         text = task.time,
